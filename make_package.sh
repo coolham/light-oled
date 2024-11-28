ee168c8269be05a1631bdf4840e560229e51a158
John Ding <python@sohu.com>
2024-11-28 09:56:07 +0800

    udpate sh

--- make_package.sh
+++ make_package.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # 使用 PyInstaller 打包 Python 程序
-pyinstaller --noconfirm --name light-oled main.py
+APP_NAME="light-oled"
+pyinstaller --noconfirm --name $APP_NAME main.py
 
 # 创建目标目录
-mkdir -p dist/light-oled/conf
-mkdir -p dist/light-oled/fonts
+mkdir -p dist/$APP_NAM/conf
+mkdir -p dist/$APP_NAM/fonts
 
 # 复制 conf 目录
-cp -r conf/* dist/light-oled/conf/
+cp -r conf/* dist/$APP_NAM/conf/
 
 # 复制 help 目录
-cp -r fonts/* dist/light-oled/fonts/
+cp -r fonts/* dist/$APP_NAM/fonts/
 
